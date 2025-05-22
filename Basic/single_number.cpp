feb05e1c05a359986e304ad92079459ad74f03cb
sherinpriyanga <46302399+sherinpriyanga@users.noreply.github.com>
2025-05-22 13:50:40 +0530

    Update single_number.cpp

--- Basic/single_number.cpp
+++ Basic/single_number.cpp
@@ -58,3 +58,37 @@ int main() {
 
     return 0;
 }
+
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int func(int n, int arr[]){
+   int num,flag;
+
+    for (int i=0;i<n;i++){
+        num=arr[i];
+           flag=0;
+//        std::cout<<"\n"<<arr[i];
+     
+        for(int j=0;j<n;j++){
+            if ((num==arr[j]) && i!=j){
+            flag=1;
+//            std::cout<<"\nFlag "<<num<<" "<<arr[j];break;
+            }
+        }   
+
+        if (flag==0)
+        return num;
+    }
+    return 0;
+}
+int main() {
+   
+    int n;
+    n=5;
+    int arr[5]={2,1,2,3,1};
+    std::cout<<func(n,arr) ;  
+
+    return 0;
+}
