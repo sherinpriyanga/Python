072558e8111f87342c82436e6dbca2d57c3158dd
sherinpriyanga <46302399+sherinpriyanga@users.noreply.github.com>
2025-05-22 13:31:53 +0530

    Update single_number.cpp

--- Basic/single_number.cpp
+++ Basic/single_number.cpp
@@ -1,27 +1,58 @@
-Single Number
+// Single Number
+
+// // Online C++ compiler to run C++ program online
+// #include <iostream>
+
+// int func(int n, int arr[]){
+//    int num; 
+//     for (int i=0;i<n;i++){
+//         num=arr[i];
+//         for(int j=1;j<n;j++){
+//             if (num==arr[j]){
+// //                std::cout<<arr[j]<<" "<<num;
+//                 break; 
+//             }
+//         }
+//     }
+//     return num;
+// }
+// int main() {
+//     // Write C++ code here
+//     std::cout << "Try programiz.pro";
+    
+//     int n;
+//     n=3;
+//     int arr[5]={2,1,3,2,1};
+//     std::cout<<func(n,arr) ;  
+
+//     return 0;
+// }
 
 // Online C++ compiler to run C++ program online
 #include <iostream>
 
 int func(int n, int arr[]){
-   int num; 
+   int num,flag;
+
     for (int i=0;i<n;i++){
         num=arr[i];
-        for(int j=1;j<n;j++){
+           flag=0;
+//        std::cout<<"\n"<<arr[i];
+        for(int j=i+1;j<n;j++){
             if (num==arr[j]){
-//                std::cout<<arr[j]<<" "<<num;
-                break; 
+            flag=1;
+//            std::cout<<"\nFlag "<<num<<" "<<arr[j];break;
             }
         }
+        if (flag==0)
+        return num;
     }
-    return num;
+    return 0;
 }
 int main() {
-    // Write C++ code here
-    std::cout << "Try programiz.pro";
-    
+   
     int n;
-    n=3;
+    n=5;
     int arr[5]={2,1,3,2,1};
     std::cout<<func(n,arr) ;  
 
